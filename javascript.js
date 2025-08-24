c3247ef07094bc69ad09ffabdd6fdf5271f86290
Sonya <sonyacqu@gmail.com>
2025-08-24 13:36:16 -0400

    feat: add getHumanChoice function

--- javascript.js
+++ javascript.js
@@ -1,30 +1,21 @@
-// function getComputerChoice() {
-//   let choice = Math.random();
-
-//   if (choice <1/3){
-//     return "rock"
-//   }
-//   else if (choice<2/3){
-//     return "paper"
-//   }
-//   else {
-//     return "scissors"
-//   }
-// }
-
-// console.log(getComputerChoice())
-
 function getComputerChoice(){
-  let choice = Math.random();
-  if (choice<1/3){
-    return "rock"
+  let computerChoice = Math.random();
+  if (computerChoice<1/3){
+    return "rock";
   }
-  else if (choice<2/3){
-    return "paper"
+  else if (computerChoice<2/3){
+    return "paper";
   }
   else {
-    return "scissors"
+    return "scissors";
   }
 }
 
 console.log(getComputerChoice())
+
+function getHumanChoice(){
+  let humanChoice = prompt("Enter your choice: Rock, Paper, or Scissors?");
+  return humanChoice;
+}
+
+console.log(getHumanChoice())
