22b2b27186387970359619f3440f404226e5e32a
Sonya <sonyacqu@gmail.com>
2025-08-27 16:34:34 -0400

    remove logic that plays five rounds and setup DOM manipulation

--- javascript.js
+++ javascript.js
@@ -18,46 +18,54 @@ function getHumanChoice(){
   return humanChoice;
 }
 
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("scissors");
 
-function playGame(){
-  let humanScore = 0;
-  let computerScore = 0;
+// before event delegation
 
-  function playRound(humanChoice, computerChoice){
-  humanChoice = humanChoice.toLowerCase()
 
-    if (humanChoice === computerChoice){
-      console.log(`It's a tie! You both chose ${humanChoice}`);
-      return "tie";
-    }
 
-    const humanWins =
-      (humanChoice === "rock" && computerChoice === "scissors") || 
-      (humanChoice === "scissors" && computerChoice === "paper") || 
-      (humanChoice === "paper" && computerChoice === "rock")
-    
-    if (humanWins) {
-      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-      return result = "human";
-    }
-    else {
-      console.log(`You lose... ${computerChoice} beats ${humanChoice}`);
-      return result = "computer";
-    }
+// function playGame(){
+//   let humanScore = 0;
+//   let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+humanChoice = humanChoice.toLowerCase()
+
+  if (humanChoice === computerChoice){
+    console.log(`It's a tie! You both chose ${humanChoice}`);
+    return "tie";
   }
+
+  const humanWins =
+    (humanChoice === "rock" && computerChoice === "scissors") || 
+    (humanChoice === "scissors" && computerChoice === "paper") || 
+    (humanChoice === "paper" && computerChoice === "rock")
   
-for (let round = 1; round <=5; round++) {
-  console.log(`Round ${round}`);
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
-  const result = playRound(humanSelection, computerSelection);
+  if (humanWins) {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    return result = "human";
+  }
+  else {
+    console.log(`You lose... ${computerChoice} beats ${humanChoice}`);
+    return result = "computer";
+  }
+}
+  
+// for (let round = 1; round <=5; round++) 
+for (let round = 1; round <= 1; round++){
+console.log(`Round ${round}`);
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+const result = playRound(humanSelection, computerSelection);
 
-  if (result === "human"){
-  humanScore++;
-  }
-  else if (result === "computer"){
-  computerScore++;
-  }
+if (result === "human"){
+humanScore++;
+}
+else if (result === "computer"){
+computerScore++;
+}
 }
 
 console.log(`Game Over!`)
@@ -72,6 +80,6 @@ else if (humanScore < computerScore) {
 else {
   console.log (`You tied! You and the computer both got: ${computerScore} points`)
 }
-}
+// }
 
-playGame();
+playRound();
