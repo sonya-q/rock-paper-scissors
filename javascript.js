9afcff1476390977ad5e87067f06dadbfb3e44fd
Sonya <sonyacqu@gmail.com>
2025-08-27 17:05:08 -0400

    get user input through button click

--- javascript.js
+++ javascript.js
@@ -10,31 +10,43 @@ function getComputerChoice(){
     return "scissors";
   }
 }
-
-console.log(getComputerChoice())
-
-function getHumanChoice(){
-  let humanChoice = prompt("Enter your choice: Rock, Paper, or Scissors?");
-  return humanChoice;
-}
+// function getHumanChoice(){
+//   let humanChoice = prompt("Enter your choice: Rock, Paper, or Scissors?");
+//   return humanChoice;
+// }
 
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
-const scissors = document.querySelector("scissors");
+const scissors = document.querySelector("#scissors");
 
 // before event delegation
 
+rock.addEventListener("click", () => {
+  const humanChoice = 'rock';
+  playRound(humanChoice, getComputerChoice())
+})
 
+paper.addEventListener("click", () => {
+  const humanChoice = 'paper';
+  playRound(humanChoice, getComputerChoice())
+})
+
+scissors.addEventListener("click", () => {
+  const humanChoice = 'scissors';
+  playRound(humanChoice, getComputerChoice())
+})
 
 // function playGame(){
 //   let humanScore = 0;
 //   let computerScore = 0;
 
+let humanScore = 0;
+let computerScore = 0;
+
 function playRound(humanChoice, computerChoice){
-humanChoice = humanChoice.toLowerCase()
 
   if (humanChoice === computerChoice){
-    console.log(`It's a tie! You both chose ${humanChoice}`);
+    console.log(`It's a tie! You both chose ${humanChoice}!`);
     return "tie";
   }
 
@@ -44,42 +56,45 @@ humanChoice = humanChoice.toLowerCase()
     (humanChoice === "paper" && computerChoice === "rock")
   
   if (humanWins) {
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+    humanScore++;
     return result = "human";
   }
   else {
-    console.log(`You lose... ${computerChoice} beats ${humanChoice}`);
+    console.log(`You lose... ${computerChoice} beats ${humanChoice}!`);
+    computerScore++;
     return result = "computer";
   }
+
 }
   
 // for (let round = 1; round <=5; round++) 
-for (let round = 1; round <= 1; round++){
-console.log(`Round ${round}`);
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-const result = playRound(humanSelection, computerSelection);
+// for (let round = 1; round <= 1; round++){
+// console.log(`Round ${round}`);
+// const computerSelection = getComputerChoice();
+// const result = playRound(humanChoice, computerSelection);
 
-if (result === "human"){
-humanScore++;
-}
-else if (result === "computer"){
-computerScore++;
-}
-}
-
-console.log(`Game Over!`)
-if (humanScore > computerScore) {
-  console.log (`Congratulations! You won! Your score is: ${humanScore} and the computer's score is ${computerScore}`)
-}
-
-else if (humanScore < computerScore) {
-  console.log (`No! You lost...Your score is: ${humanScore} and the computer's score is ${computerScore}`)
-}
-
-else {
-  console.log (`You tied! You and the computer both got: ${computerScore} points`)
-}
+// if (result === "human"){
+// humanScore++;
+// }
+// else if (result === "computer"){
+// computerScore++;
 // }
 
-playRound();
+function showScores(){
+  console.log(`Your score: ${humanScore}; Computer score: ${computerScore}`)
+}
+
+// console.log(`Game Over!`)
+// if (humanScore > computerScore) {
+//   console.log (`Congratulations! You won! Your score is: ${humanScore} and the computer's score is ${computerScore}`)
+// }
+
+// else if (humanScore < computerScore) {
+//   console.log (`No! You lost...Your score is: ${humanScore} and the computer's score is ${computerScore}`)
+// }
+
+// else {
+//   console.log (`You tied! You and the computer both got: ${computerScore} points`)
+// }
+// }
